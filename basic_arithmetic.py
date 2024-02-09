f22a8a8d770e38a173d545caaff1589e8f71626d
heisenberg0908 <heisenberg0908>
2024-02-09 23:39:38 +0530

    pyhton basics programs

---
+++ basic_arithmetic.py
@@ -0,0 +1,23 @@
+# to find sum:
+def sum(a,b):
+    return a+b
+
+# to find product:
+def product(a,b):
+    return a*b
+
+# division:
+def divide(a,b):
+    return a/b
+
+# to find exponential:
+def expo(a,b):
+    return a**b
+
+# to find remainder
+def remainder(a,b):
+    return a%b
+
+# to find round of quotient(division):
+def divide2(a,b):
+    return a//b
